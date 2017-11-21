ad76d51707792ba2af7b39312f06f31ce7abfe50
Cliff Parnitzky <github@cliff-parnitzky.de>
2017-11-21 23:39:00 +0100

    Correctly require `initialize.php`

--- system/modules/MonitoringClient/api/api.php
+++ system/modules/MonitoringClient/api/api.php
@@ -8,10 +8,17 @@
  * @license LGPL-3.0+
  */
 
-// Needed for Contao 4
-define('TL_SCRIPT', ""); 
- // Initialize the system
-require dirname(__DIR__) . '../../../../system/initialize.php';
+// Initialize the system
+if (file_exists(dirname(__DIR__) . '/../../../../../../system/initialize.php'))
+{
+  // Contao 4 environment
+  define('TL_SCRIPT', ""); // Only needed for Contao 4
+  require dirname(__DIR__) . '/../../../../../../system/initialize.php';
+}
+else
+{
+  require dirname(__DIR__) . '/../../../system/initialize.php';
+}
 
 // Get the data
 $client = new MonitoringClient();
